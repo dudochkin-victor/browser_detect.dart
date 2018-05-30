0cb28b4c56160266f7b2a9a259207ae1204ae147
Victor Dudochkin <dudochkin.victor@gmail.com>
2018-05-30 23:15:52 +0700

    Dart2 changes

--- test/browser_detect_tests.dart
+++ test/browser_detect_tests.dart
@@ -1,6 +1,6 @@
 library browser_detect_tests;
 
-import 'package:unittest/unittest.dart';
+// import 'package:unittest/unittest.dart';
 import 'package:unittest/html_enhanced_config.dart';
 
 import 'browser_version_tests.dart';
